378810e8237f7e1d7da839674129fd75b68cbe9c
Haider <konnect2haider@gmail.com>
2019-08-07 22:15:30 +0500

    Validation for Users Model Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true, uniqueness: {case_sensitive: false} , length: {minimum: 3,maximum: 20}
+  VALID_EMAIL_REGEX = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/i
+   validates :email, presence: true,uniqueness: {case_sensitive: false}, length: {minimum: 5,maximum: 50},
+              format: {with: VALID_EMAIL_REGEX}
 end
